1da7777262555e81e2bda82eb13ffdb2108d7ab3
Pjinha <qq0201@naver.com>
2024-06-09 16:49:00 +0900

    modify

--- src/component/GanttComp.js
+++ src/component/GanttComp.js
@@ -1,10 +1,17 @@
 import React, {useCallback, useEffect, useRef, useState} from 'react';
 import '../public/css/ganttchart.css';
 import moment from 'moment';
+import IconButton from "@mui/material/IconButton";
+import Box from "@mui/material/Box";
+import {ChevronLeft} from "@mui/icons-material";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
 const GanttChart = ({tasks, onTaskClick}) => {
-    const chartStartDate = moment().subtract(1, 'days');
-    const chartEndDate = moment().add(13, 'days');
+    // const chartStartDate = moment().subtract(1, 'days');
+    // const chartEndDate = moment().add(13, 'days');
+    const [chartStartDate, setChartStartDate] = useState(moment().subtract(1, 'days'));
+    const [chartEndDate, setChartEndDate] = useState(moment().add(13, 'days'));
     const chartDays = chartEndDate.diff(chartStartDate, 'days');
     const parentRef = useRef(null);
 
@@ -37,60 +44,79 @@ const GanttChart = ({tasks, onTaskClick}) => {
     }, [updateSkeletonRows]);
 
     return (
-        <div className="gantt-chart">
-            <table className="gantt-chart-table">
-                <thead>
-                <tr>
-                    <th className="gantt-task-name-header">Task</th>
-                    {[...Array(chartDays).keys()].map(day => (
-                        <th key={day} className="gantt-chart-day">
-                            {moment(chartStartDate).add(day, 'days').format('MM/DD')}
-                        </th>
-                    ))}
-                </tr>
-                </thead>
-                <tbody>
-                {tasks.map((task, index) => {
-                    let taskOffset = getDayOffset(task.start);
-                    const taskDuration = getDayOffset(task.end) - taskOffset + 1;
-                    const taskCompleteDays = Math.floor((taskDuration * task.progress) / 100);
-                    // console.log(task.start, task.end, taskOffset, taskDuration, taskCompleteDays);
-                    return (
-                        <tr key={index}>
-                            <td className="gantt-task-name">{task.title}</td>
-                            {[...Array(chartDays).keys()].map(day => {
-                                const isTaskDay = day >= taskOffset && day < taskOffset + taskDuration;
-                                const isCompleteDay = day >= taskOffset && day < taskOffset + taskCompleteDays;
-                                return (
-                                    <td
-                                        key={day}
-                                        className={`gantt-task-cell ${isTaskDay ? 'task-day' : ''}`}
-                                        style={{
-                                            backgroundColor: isCompleteDay ? 'rgba(0, 128, 0, 0.5)' :
-                                                isTaskDay ? 'rgba(0, 128, 0, 0.1)' : 'transparent'
-                                        }}
-                                        onClick={() => {
-                                            if (isTaskDay) {
-                                                onTaskClick(task);
-                                                console.log('Clicked on', task.title, 'on', moment(chartStartDate).add(day, 'days').format('MM/DD'));
-                                            }
-                                        }}
-                                    >
-                                        {isTaskDay && day === taskOffset && (
-                                            <div className="gantt-task-inner">
-                                                {task.progress}%
-                                            </div>
-                                        )}
-                                    </td>
-                                );
-                            })}
-                        </tr>
-                    );
-                })}
-                {skeletonRows}
-                </tbody>
-            </table>
-        </div>
+        <Box display="flex" flexDirection={"column"} width={"90%"}>
+            <Box className={"gantt-header"}>
+                <IconButton className={"gantt-header-icon"} onClick={() => {
+                    setChartStartDate(moment(chartStartDate).subtract(1, 'week'));
+                    setChartEndDate(moment(chartEndDate).subtract(1, 'week'));
+                }}>
+                    <ChevronLeftIcon />
+                </IconButton>
+                <div className={"gantt-header-title"}>
+                    {chartStartDate.format('MM/DD')} - {chartEndDate.format('MM/DD')}
+                </div>
+                <IconButton className={"gantt-header-icon"} onClick={() =>{
+                    setChartStartDate(moment(chartStartDate).add(1, 'week'));
+                    setChartEndDate(moment(chartEndDate).add(1, 'week'));
+                }}>
+                    <ChevronRightIcon />
+                </IconButton>
+            </Box>
+            <div className="gantt-chart">
+                <table className="gantt-chart-table">
+                    <thead>
+                    <tr>
+                        <th className="gantt-task-name-header">Task</th>
+                        {[...Array(chartDays).keys()].map(day => (
+                            <th key={day} className="gantt-chart-day">
+                                {moment(chartStartDate).add(day, 'days').format('MM/DD')}
+                            </th>
+                        ))}
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {tasks.map((task, index) => {
+                        let taskOffset = getDayOffset(task.start);
+                        const taskDuration = getDayOffset(task.end) - taskOffset + 1;
+                        const taskCompleteDays = Math.floor((taskDuration * task.progress) / 100);
+                        // console.log(task.start, task.end, taskOffset, taskDuration, taskCompleteDays);
+                        return (
+                            <tr key={index}>
+                                <td className="gantt-task-name">{task.title}</td>
+                                {[...Array(chartDays).keys()].map(day => {
+                                    const isTaskDay = day >= taskOffset && day < taskOffset + taskDuration;
+                                    const isCompleteDay = day >= taskOffset && day < taskOffset + taskCompleteDays;
+                                    return (
+                                        <td
+                                            key={day}
+                                            className={`gantt-task-cell ${isTaskDay ? 'task-day' : ''}`}
+                                            style={{
+                                                backgroundColor: isCompleteDay ? 'rgba(0, 128, 0, 0.5)' :
+                                                    isTaskDay ? 'rgba(0, 128, 0, 0.1)' : 'transparent'
+                                            }}
+                                            onClick={() => {
+                                                if (isTaskDay) {
+                                                    onTaskClick(task);
+                                                    console.log('Clicked on', task.title, 'on', moment(chartStartDate).add(day, 'days').format('MM/DD'));
+                                                }
+                                            }}
+                                        >
+                                            {isTaskDay && day === taskOffset && (
+                                                <div className="gantt-task-inner">
+                                                    {task.progress}%
+                                                </div>
+                                            )}
+                                        </td>
+                                    );
+                                })}
+                            </tr>
+                        );
+                    })}
+                    {skeletonRows}
+                    </tbody>
+                </table>
+            </div>
+        </Box>
     );
 };
 
